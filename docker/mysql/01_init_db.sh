56a1db7cf3569ddc86c621bf0b448ccbb669c6ae
Panzer Punk <gsuper36@gmail.com>
2026-07-25 15:45:37 +0300

    working mysql

--- docker/mysql/01_init_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mysql -u root -p${DB_ROOT_PASSWORD} <<EOF
-create database live;
-grant all on live.* to '${DB_USER}'@'localhost' identified by '${DB_PASSWORD}';
-grant all on live.* to '${DB_USER}'@'localhost' identified by '${DB_PASSWORD}';
-EOF
